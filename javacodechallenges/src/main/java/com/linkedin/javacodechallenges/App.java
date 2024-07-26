1cebe0f88de1329bf51c239dfe91c195bc83fb52
rabukakeher <62230043+rabukakeher@users.noreply.github.com>
2024-07-26 02:45:24 +0000

    Average Change Invested

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,10 +1,16 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.List;
+import java.util.Optional;
 
 public class App {
     public static double calculateAverageChangeInvested(List<Double> purchases) {
-        return 0;
+        int numberOfPurchases = purchases.size();
+        Optional<Double> totalInvested = purchases.stream()
+         .map(value -> 1 - (value - value.longValue()))
+         .reduce((value1, value2) -> value1 + value2);
+
+         return totalInvested.isPresent() ? (totalInvested.get()/numberOfPurchases) : 0;
     }
 
     public static void main(String[] args) {

478914b27ed824a3bb05d9a509891ef845169806
rabukakeher <62230043+rabukakeher@users.noreply.github.com>
2024-07-27 16:44:57 +0000

    joke generator

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,11 +1,41 @@
 package com.linkedin.javacodechallenges;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException{
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                        .uri(URI.create("https://icanhazdadjoke.com/"))
+                        .header("Content-Type", "application/html").build();
+        HttpClient httpClient = HttpClient.newBuilder().build();
+
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, BodyHandlers.ofString());
+
+        String resposeBody = httpResponse.body();
+
+        Pattern p = Pattern.compile("<meta property=\"og:description\" content=\"(.*)\" \\/>");
+
+        Matcher m = p.matcher(resposeBody);
+
+        if(m.find()) {
+           System.out.println(m.group(1));
+        }
+        
+
         // TODO: Call https://icanhazdadjoke.com/ API and display joke
     }
+
+
 }
